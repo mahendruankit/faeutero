04b73fcfca05630c954ab930f5682557bcef09c6
Ankit Mahendru <ankit.mahendru@gmail.com>
2024-03-04 20:16:55 +0000

    change insta handle

--- src/resources/strings.js
+++ src/resources/strings.js
@@ -23,7 +23,7 @@ const strings = {
       spotify: 'https://open.spotify.com/album/0Kn6DjslFyS4Esu1yZvOoS',
       applemusic: 'https://music.apple.com/gb/album/hill-of-beans/1710124819',
       bandcamp: 'https://fae-utero.bandcamp.com/album/hill-of-beans',
-      instagram: 'https://www.instagram.com/fae.utero/',
+      instagram: 'https://www.instagram.com/anonymahendru/',
     },
     gdpr: 'Just a moment! This website uses cookies to enhance the user experience. If you continue to browse this site, you are agreeing to our use of cookies. You can disable cookies at any time within your browser settings.',
     gdprbutton: 'Aight bro',
